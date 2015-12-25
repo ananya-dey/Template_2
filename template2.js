5635c6eea54dcacd5d91cff3a3c43ecda2ee5508
anna67 <ananyadey067@gmail.com>
2015-12-24 22:47:50 -0800

    It is a one page template
    using html5,css3,javascript ,bootstrap,jquery

---
+++ template2.js
@@ -0,0 +1,58 @@
+
+    $(document).ready(function(){
+      $('.carousel').slick({
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  autoplay: true,
+		  autoplaySpeed: 2000,
+		 
+		});
+    });
+
+ 
+    $(document).ready(function(){
+      $('.carousel2').slick({
+		  slidesToShow: 4,
+		  slidesToScroll: 1,
+		  autoplay: false,
+		  autoplaySpeed: 2000,
+		});
+    });
+
+ 
+    $(document).ready(function(){
+      $('.carousel3').slick({
+		autoplay: true,
+	  });
+    });
+ 
+    $(document).ready(function(){
+      $('.carousel4').slick({
+		  slidesToShow: 4,
+		  slidesToScroll: 1,
+		  autoplay: false,
+		  autoplaySpeed: 2000,
+		 
+		});
+    });
+ 
+   
+        $('.img-holder').imageScroll({
+//            image: null,
+//            imageAttribute: 'image',
+//            container: $('body'),
+//            windowObject: $(window),
+//            speed:.2,
+//            coverRatio:.75,
+//            coverRatio:1,
+//            holderClass: 'imageHolder',
+//            imgClass: 'img-holder-img',
+//            holderMinHeight: 200,
+//            holderMaxHeight: null,
+//            extraHeight: 50,
+//            mediaWidth: 800,
+//            mediaHeight: 450,
+//            parallax: true,
+//            touch: false
+        });
+    
